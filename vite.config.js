505179a3f486435f39b5ae5ac08fa77536ea02a4
”Jakob <jako500n@stud.kea.dk>
2022-12-16 07:33:23 +0100

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,14 @@ export default defineConfig({
     outDir: resolve("dist"),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        // about: resolve(__dirname, "src/about.html"),
+        index: resolve(__dirname, "src/index.html"),
+        komponenter: resolve(__dirname, "src/komponenter.html"),
+        design_selv: resolve(__dirname, "src/design_selv.html"),
+        gavekort: resolve(__dirname, "src/gavekort.html"),
+        indkoebskurv: resolve(__dirname, "src/indkoebskurv.html"),
+        om_julie: resolve(__dirname, "src/om_julie.html"),
+        shop: resolve(__dirname, "src/shop.html"),
+        single_view: resolve(__dirname, "src/single_view.html"),
       },
     },
   },
