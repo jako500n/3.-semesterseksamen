e07445afb999e1dbcadaf590cd1463a1831226b6
”Jakob <jako500n@stud.kea.dk>
2022-12-19 18:20:31 +0100

    købsproces (måske) done?:)

--- vite.config.js
+++ vite.config.js
@@ -14,13 +14,25 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, "src/index.html"),
-        komponenter: resolve(__dirname, "src/komponenter.html"),
+        betalt: resolve(__dirname, "src/betalt.html"),
         design_selv: resolve(__dirname, "src/design_selv.html"),
         gavekort: resolve(__dirname, "src/gavekort.html"),
         indkoebskurv: resolve(__dirname, "src/indkoebskurv.html"),
+        koeb_leveringsmetode: resolve(
+          __dirname,
+          "src/koeb_leveringsmetode.html"
+        ),
+        koeb_oplysninger: resolve(__dirname, "src/koeb_oplysninger.html"),
+        koebsproces_afhent_01: resolve(
+          __dirname,
+          "src/koebsproces_afhent_01.html"
+        ),
+        koebsproces_post_01: resolve(__dirname, "src/koebsproces_post_01.html"),
+        komponenter: resolve(__dirname, "src/komponenter.html"),
         om_julie: resolve(__dirname, "src/om_julie.html"),
         shop: resolve(__dirname, "src/shop.html"),
         single_view: resolve(__dirname, "src/single_view.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
       },
     },
   },
